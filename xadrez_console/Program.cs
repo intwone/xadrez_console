814f04e580a94179ed8d64c7ddb637ba0ee2882f
Cassio Oliveira <cassiointw1993@gmail.com>
2019-03-16 17:29:38 -0300

    Criando a classe PosicaoXadrez

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -6,20 +6,11 @@ namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
 
-            try { // Tenta executar
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-                
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e) { // Se der algum erro
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos);
 
-            Console.WriteLine();
+            Console.WriteLine(pos.toPosicao());
         }
     }
 }
---
+++ xadrez_console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,23 @@
+﻿using tabuleiro;
+
+namespace xadrez {
+    class PosicaoXadrez {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        // Método de conversão das casas do jogo de xadrez
+        public Posicao toPosicao() {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString() {
+            return "" + coluna + linha;
+        }
+
+    }
+}
